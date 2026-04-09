410a16a6f06cc28f15bac3fac3453ebe3786963e
Sheena <sheenamelgar22gmail@gmail.com>
2026-04-09 08:21:53 +0800

    message

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function forgotPassword(){
+  const email = prompt("Enter your email:");
 
-// Write your JavaScript code.
+  if(!email){
+    alert("Email is required");
+    return;
+  }
+
+  alert("Reset link sent to " + email);
+}
